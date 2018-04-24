34a6ba4810ad52f873db0582c03e8e3a7d2685cd
user0189 <38674413+user0189@users.noreply.github.com>
2018-04-24 02:49:25 -0400

    Creando periodico en base a la descripcion
    Los datos que se requieren almacenar para cada uno de los periódicos son:
    • Clave de periodico: Este campo define el orden en el ABB.
    • Nombre
    • Tipo: Que puede ser impreso / on-line.
    • Top de noticias más leídas o comentadas por día. Para la implementación de esta sección
    se deberá implementar una lista ligada ordenada por fecha, que contendrá las noticias
    más leídas o comentadas por día. La información que se debe almacenar para cada
    noticia es:
    • Fecha (Factor de orden de la lista ligada)
    • Encabezado de la noticia
    • Nombre del periodista

--- periodico.java
+++ periodico.java
@@ -1,4 +1,22 @@
 
-public class periodico {
 
+public class periodico {
+	public int clave;
+	public String Nombre;
+	public String Tipo;
+	
+	public periodico(int c, String n, String t){
+		this.clave = c;
+		this.Nombre = n;
+		this.Tipo = t;
+	}
+	
+	public void modificarNombre(String n) {
+		this.Nombre = n;
+	}
+	
+	public void modificarTipo(String t) {
+		this.Tipo = t;
+	}
+		
 }
