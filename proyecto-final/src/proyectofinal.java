f701c2e18713d20da39a27f357c652a5356f404e
user0189 <38674413+user0189@users.noreply.github.com>
2018-04-26 02:38:18 -0400

    Menu

--- proyecto-final/src/proyectofinal.java
+++ proyecto-final/src/proyectofinal.java
@@ -1,9 +1,90 @@
 
+import java.util.Scanner;
 public class proyectofinal {
 
 public static void main(String[] args) {
 	
+	Scanner entrada = new Scanner(System.in);
+	Scanner entrada2 = new Scanner(System.in);
+	Scanner entrada3 = new Scanner(System.in);
 	
-
+	int op = 0;
+	int op2 = 0;
+	int op3 = 0;
+	
+	while(op != 9) {
+	System.out.println("1) Insertar");
+	System.out.println("2) Modificar");
+	System.out.println("3) Buscar");
+	System.out.println("4) Exportar en preorden");
+	System.out.println("5) Exportar en inorden");
+	System.out.println("6) Exportar en postorden");
+	System.out.println("7) Exportar periodico");
+	System.out.println("8) Eliminar");
+	System.out.println("9) Salir");
+	
+	op = entrada.nextInt();
+	
+	switch(op) {
+	case 1:
+		break;
+	case 2:
+	while(op2 != 4 ) {
+		System.out.println("1) Nombre del periodico");
+		System.out.println("2) Tipo");
+		System.out.println("3) Top noticias");
+		System.out.println("4) Regresar al menu principal");
+		op2 = entrada2.nextInt();
+		
+		switch(op2) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			while(op3 != 6) {
+				System.out.println("1) Insertar por fecha");
+				System.out.println("2) Eliminar por fecha");
+				System.out.println("3) Modificar");
+				System.out.println("4) Mostrat por dia");
+				System.out.println("5) Mostrar todos");
+				System.out.println("6) Regresar al menu anterior");
+				op3 = entrada3.nextInt();
+			switch(op3) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+			}
+			}
+			break;
+		}
+		
+	}
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	case 8:
+		break;
+	case 9:
+		break;
+	}
+	
+	}
+	
 }
 }
